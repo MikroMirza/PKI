01f47244fc1494e7f04300afe42771980526b64d
MilicMilosRS <milicmilos.rs@gmail.com>
2025-09-28 15:13:24 +0200

    [FIX] Auth error not thrown for no reason anymore

--- PKI/src/main/java/rs/tim33/PKI/Exceptions/GlobalExceptionHandler.java
+++ PKI/src/main/java/rs/tim33/PKI/Exceptions/GlobalExceptionHandler.java
@@ -2,6 +2,8 @@ package rs.tim33.PKI.Exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -14,6 +16,20 @@ public class GlobalExceptionHandler {
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
                 .body(new ErrorMessage(ex.getMessage(), ex.getErrorCode()));
+    }
+    
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorMessage> handleInvalidJson(HttpMessageNotReadableException ex) {
+        return ResponseEntity
+                .badRequest()
+                .body(new ErrorMessage(ex.getMessage(), "MSG_NOT_READABLE"));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorMessage> handleValidationErrors(MethodArgumentNotValidException ex) {
+        return ResponseEntity
+                .badRequest()
+                .body(new ErrorMessage(ex.getMessage(), "ARG_INVALID"));
     }
 }
 
